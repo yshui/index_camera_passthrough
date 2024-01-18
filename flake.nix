7f9485f4f47f5f332a370473f311ffc36e8596f2
Yuxuan Shui <yshuiv7@gmail.com>
2024-01-18 01:17:15 +0000

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.fenix = {
+    inputs.nixpkgs.follows = "nixpkgs";
+    url = github:nix-community/fenix;
+  };
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs, fenix }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; overlays = [ fenix.overlays.default ]; };
+    sources = import ./nix/sources.nix;
+    rust-toolchain = pkgs.fenix.fromToolchainFile {
+      file = ./rust-toolchain.toml;
+      sha256 = sources.channel-rust-nightly.sha256;
+    };
+  in with pkgs; {
+    devShells.${system}.default = mkShell {
+      nativeBuildInputs = [ pkg-config cmake rust-toolchain ];
+      buildInputs = [ systemdLibs linuxHeaders openvr ];
+      shellHook = ''
+        export LD_LIBRARY_PATH="${lib.makeLibraryPath [ libglvnd vulkan-loader ]}:$LD_LIBRARY_PATH"
+      '';
+      LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages_17.libclang.lib ];
+
+      BINDGEN_EXTRA_CLANG_ARGS =
+      # Includes with normal include path
+      (builtins.map (a: ''-I"${a}/include"'') [
+        # add dev libraries here (e.g. pkgs.libvmi.dev)
+        linuxHeaders
+      ]) ++ [
+        ''-isystem "${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${lib.versions.major pkgs.llvmPackages_latest.libclang.version}/include"''
+        ''-isystem "${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}"''
+        ''-isystem "${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config}"''
+        ''-isystem "${glibc.dev}/include"''
+      ];
+    };
+  };
+}
