5a3d1dcc9f9179289e8edad0058617ff8a020d03
Yuxuan Shui <yshuiv7@gmail.com>
2024-02-28 23:17:21 +0000

    fix device memory not mapped error

--- src/utils.rs
+++ src/utils.rs
@@ -2,11 +2,13 @@ use std::sync::Arc;
 
 use vulkano::{
     buffer::{AllocateBufferError, Buffer, BufferCreateInfo, RawBuffer},
+    command_buffer::ResourceUseRef,
     device::Device,
     image::{sys::RawImage, AllocateImageError, Image, ImageCreateFlags, ImageCreateInfo},
     memory::{
         allocator::{MemoryAllocatorError, MemoryTypeFilter},
-        DedicatedAllocation, DeviceMemory, MemoryAllocateInfo, MemoryRequirements, ResourceMemory,
+        DedicatedAllocation, DeviceMemory, MemoryAllocateInfo, MemoryMapInfo, MemoryRequirements,
+        ResourceMemory,
     },
     Validated,
 };
@@ -63,12 +65,13 @@ fn allocate_dedicated(
     device: Arc<Device>,
     dedicate_allocation: DedicatedAllocation<'_>,
     filter: MemoryTypeFilter,
+    should_map: bool,
 ) -> Result<ResourceMemory, MemoryAllocatorError> {
     let memory_requirements = dedicated_allocation_memory_requirements(dedicate_allocation);
     let memory_type_index =
         find_memory_type_index(device.clone(), memory_requirements.memory_type_bits, filter)
             .ok_or(MemoryAllocatorError::FindMemoryType)?;
-    let device_memory = DeviceMemory::allocate(
+    let mut device_memory = DeviceMemory::allocate(
         device.clone(),
         MemoryAllocateInfo {
             allocation_size: memory_requirements.layout.size(),
@@ -78,6 +81,15 @@ fn allocate_dedicated(
         },
     )
     .map_err(MemoryAllocatorError::AllocateDeviceMemory)?;
+    if should_map {
+        device_memory
+            .map(MemoryMapInfo {
+                offset: 0,
+                size: device_memory.allocation_size(),
+                ..Default::default()
+            })
+            .map_err(MemoryAllocatorError::AllocateDeviceMemory)?;
+    }
     Ok(ResourceMemory::new_dedicated(device_memory))
 }
 
@@ -90,9 +102,13 @@ impl DeviceExt for Device {
         assert!(!create_info.flags.intersects(ImageCreateFlags::DISJOINT));
         let raw_image = RawImage::new(self.clone(), create_info)
             .map_err(|x| x.map(AllocateImageError::CreateImage))?;
-        let resource_memory =
-            allocate_dedicated(self.clone(), DedicatedAllocation::Image(&raw_image), filter)
-                .map_err(|x| Validated::Error(AllocateImageError::AllocateMemory(x)))?;
+        let resource_memory = allocate_dedicated(
+            self.clone(),
+            DedicatedAllocation::Image(&raw_image),
+            filter,
+            false,
+        )
+        .map_err(|x| Validated::Error(AllocateImageError::AllocateMemory(x)))?;
         unsafe { raw_image.bind_memory(Some(resource_memory)) }
             .map_err(|(x, _, _)| x.map(AllocateImageError::BindMemory))
             .map(Arc::new)
@@ -104,9 +120,13 @@ impl DeviceExt for Device {
     ) -> Result<Arc<Buffer>, Validated<AllocateBufferError>> {
         let buffer = RawBuffer::new(self.clone(), create_info)
             .map_err(|x| x.map(AllocateBufferError::CreateBuffer))?;
-        let resource_memory =
-            allocate_dedicated(self.clone(), DedicatedAllocation::Buffer(&buffer), filter)
-                .map_err(|x| Validated::Error(AllocateBufferError::AllocateMemory(x)))?;
+        let resource_memory = allocate_dedicated(
+            self.clone(),
+            DedicatedAllocation::Buffer(&buffer),
+            filter,
+            true,
+        )
+        .map_err(|x| Validated::Error(AllocateBufferError::AllocateMemory(x)))?;
         unsafe { buffer.bind_memory(resource_memory) }
             .map_err(|(x, _, _)| x.map(AllocateBufferError::BindMemory))
             .map(Arc::new)
